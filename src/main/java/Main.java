bf4b9d30cacd3807e49360d11d6d9db66899b01f
Alfina332 <104842876+Alfina332@users.noreply.github.com>
2022-07-02 20:31:09 +0500

    Add files via upload

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import com.google.gson.Gson;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
+        System.out.println(engine.search("бизнес"));
+
+        try (ServerSocket serverSocket = new ServerSocket(8989);) { // стартуем сервер один(!) раз
+            while (true) { // в цикле(!) принимаем подключения
+                try (Socket socket = serverSocket.accept();
+                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                     PrintWriter out = new PrintWriter(socket.getOutputStream());) {
+                    String word = in.readLine();
+                    String json = new Gson().toJson(engine.search(word));
+                    out.write(json);
+                    out.flush();
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Не могу стартовать сервер");
+            e.printStackTrace();
+        }
+    }
+}
